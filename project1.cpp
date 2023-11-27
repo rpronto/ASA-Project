623998b9f30e6a6eb13ce2968b2a0da99df9e1f1
João Martins <joao.bernardo.mota.martins@tecnico.ulisboa.pt>
2023-11-27 19:49:11 +0000

    proj1 (no memory limit exceeded)

--- project1.cpp
+++ project1.cpp
@@ -9,38 +9,37 @@ using namespace std;
 
 
 int  cut (int X, int Y, vector<int>& piece_x, vector<int>& piece_y, vector<int>& v, int n) {
-    
-    vector<vector<vector<int>>> k(X+1, vector<vector<int>>(Y+1, vector<int>(n+1, 0)));
-    for (int x = 1; x <= X; x++) {
-        for(int y = 1; y <= Y; y++ ) {
-            for(int i = 1; i <= n; i++) {
+    vector<vector<int>> k(X+1, vector<int>(Y+1, 0));
+    for (int i = 1; i <= n; i++) {
+        for(int x = 1; x <= X; x++) {
+            for(int y = 1; y <= Y; y++) {
                 if(((piece_x[i] > x) || (piece_y[i] > y)) && ((piece_x[i] > y) || (piece_y[i] > x))){
-                    k[x][y][i] = k[x][y][i-1];
+                    k[x][y] = k[x][y];
                     continue;
                 } 
                 int normal = 0;
                 int flip = 0;
                 if((piece_x[i] <= x) && (piece_y[i] <= y)) {
-                    int temp1 =  max(k[x][y][i-1],
-                    v[i] + k[x - piece_x[i]][y][i] + k[piece_x[i]][y - piece_y[i]][i]);
-                    int temp2 =  max(k[x][y][i-1],
-                    v[i] + k[x][y - piece_y[i]][i] +  k[x - piece_x[i]][piece_y[i]][i]);
+                    int temp1 =  max(k[x][y],
+                    v[i] + k[x - piece_x[i]][y] + k[piece_x[i]][y - piece_y[i]]);
+                    int temp2 =  max(k[x][y],
+                    v[i] + k[x][y - piece_y[i]] +  k[x - piece_x[i]][piece_y[i]]);
                     normal = max(temp1,temp2);
                 } if((piece_y[i] <= x) && (piece_x[i] <= y)) {
                     int aux = piece_x[i];
                     piece_x[i] = piece_y[i];
                     piece_y[i] = aux;
-                    int temp1 =  max(k[x][y][i-1],
-                    v[i] + k[x - piece_x[i]][y][i] + k[piece_x[i]][y - piece_y[i]][i]);
-                    int temp2 =  max(k[x][y][i-1],
-                    v[i] + k[x][y - piece_y[i]][i] +  k[x - piece_x[i]][piece_y[i]][i]);
+                    int temp1 =  max(k[x][y],
+                    v[i] + k[x - piece_x[i]][y] + k[piece_x[i]][y - piece_y[i]]);
+                    int temp2 =  max(k[x][y],
+                    v[i] + k[x][y - piece_y[i]] +  k[x - piece_x[i]][piece_y[i]]);
                     flip = max(temp1,temp2);
                 } 
-                k[x][y][i] = max(normal, flip);
+                k[x][y] = max(normal, flip);
             }
         }
     }
-    return k[X][Y][n];
+    return k[X][Y];
 }
 
 int main() {
