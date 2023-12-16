5cc5fd70d001fffa79635c8d684f43b66b353ace
Rafael Pronto <rafael.pronto@tecnico.ulisboa.pt>
2023-12-16 13:47:15 +0000

    proj2

--- Project_2/project2.cpp
+++ Project_2/project2.cpp
@@ -6,6 +6,18 @@
 
 using namespace std;
 
+struct Vertice {
+    int color; // 0 - Branco , 1 - Cinzento, 2 - Preto
+    int value;
+    list<Vertice> verticesAdjacentes;
+};
+
+
+
+
+
+
+
 int main() {
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL);
@@ -13,13 +25,20 @@ int main() {
     int n, m, x ,y;
     cin >> n >> m;
 
-    vector<list<int>> listAdj(n+1);
+    vector<Vertice> listAdj(n+1);
 
+    for(int i = 1; i <=n ; i++) 
+        listAdj[i].value = i;
+    
     for(int i = 0; i < m; i++) {
         cin >> x >> y;
-        listAdj[x].push_back(y);
+
+        Vertice verticeY;
+        verticeY.value = y;
+        verticeY.color = 0;
+        
+        listAdj[x].verticesAdjacentes.push_back(verticeY);
     }
 
-   
     return 0;
 }
