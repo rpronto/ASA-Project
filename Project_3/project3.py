a43071b7306fe5d2f9687bfe11c2a04838fdc53a
João Martins <joao.bernardo.mota.martins@tecnico.ulisboa.pt>
2023-12-22 17:19:31 +0000

    proj3

--- Project_3/project3.py
+++ Project_3/project3.py
@@ -1,1 +1,19 @@
 # Project2 - al014 , ist1106819 , ist1105672
+from pulp import *
+
+t, p, max = input().split()
+
+t = int(t)
+p = int(p)
+max = int(max)
+
+toys = []
+
+for n in range(t):
+    l, c = input().split()
+    toys += [[int(l),int(c)]]
+
+packages = []
+for n in range(p):
+    i, j, k, l = input().split()
+    packages += [[int(i),int(j),int(k),int(l)]]
