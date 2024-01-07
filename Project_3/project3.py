10ad928c4b52c2e6abceaa4c0cf2e4d2dbb38938
Rafael Pronto <rafael.pronto@tecnico.ulisboa.pt>
2024-01-07 13:55:17 +0000

    proj3

--- Project_3/project3.py
+++ Project_3/project3.py
@@ -6,14 +6,19 @@ t, p, max = map(int, sys.stdin.readline().split())
 lucro_t = {}
 capacidade_t = {}
 toys_p = {}
+vars_t = {}
+
+prob = LpProblem("MaxProfitToys", LpMaximize)
 
 for n in range(t):
     l, c = map(int, sys.stdin.readline().split())
     lucro_t[n] = l
     capacidade_t[n] = c
     toys_p[n] = []
+    vars_t[n]= LpVariable("toys_{}".format(n), 0, max, LpInteger)
 
 lucro_p = {}
+vars_p = {}
 
 for n in range(p):
     i, j, k, l = map(int, sys.stdin.readline().split())
@@ -21,11 +26,8 @@ for n in range(p):
     toys_p[i-1] += [n]
     toys_p[j-1] += [n]
     toys_p[k-1] += [n]
+    vars_p[n] = LpVariable("packages_{}".format(n), 0, max, LpInteger)
 
-prob = LpProblem("MaxProfitToys", LpMaximize)
-
-vars_t = LpVariable.dict("toys", list(range(t)), 0, max, LpInteger)
-vars_p = LpVariable.dict("packages", list(range(p)), 0, max, LpInteger)
 
 prob += (
     lpSum([vars_t[i] * lucro_t[i] for i in range(t)])
@@ -42,6 +44,6 @@ for i in range(t):
     prob += vars_t[i] + lpSum(vars_p[p] for p in toys_p[i]) <= capacidade_t[i]
 
 
-prob.solve(PULP_CBC_CMD(msg=0))
+prob.solve(PULP_CBC_CMD(msg=0, mip=True, timeLimit=0.01, gapRel=0.01))
 
 print(int(value(prob.objective)))
